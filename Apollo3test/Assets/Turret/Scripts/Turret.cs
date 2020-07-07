bbf70ea53567a3ae56bc21bb4c31c4aaf6735601
kamfel <kamil.fel76@gmail.com>
2020-07-07 17:48:19 +0200

    Add turret prefab
    Added turret prefab and one sample turret on player spawn.

---
+++ Apollo3test/Assets/Turret/Scripts/Turret.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    private bool isPlayerInRange;
+    private bool isShotReady = true;
+    private GameObject target;
+
+    /// <summary>
+    /// Defines how fast the turret can rotate towards player.
+    /// Added to turret's barrel rotation once per frame.
+    /// </summary>
+    public float RotationAngularSpeed;
+
+    /// <summary>
+    /// Maximum angular offset, that the gun will shoot at the player.
+    /// Sori for bad inglisz.
+    /// </summary>
+    public float ShootingAngle;
+
+    /// <summary>
+    /// Reference to turrets shooting part.
+    /// </summary>
+    public GameObject TurretGun;
+
+    /// <summary>
+    /// Projectile prefab that the gun shoots with.
+    /// </summary>
+    public GameObject Projectile;
+
+    /// <summary>
+    /// Representation of projectile spawn point.
+    /// </summary>
+    public GameObject ProjectileSpawnPoint;
+
+    /// <summary>
+    /// Firing speed.
+    /// </summary>
+    public float FiringSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPlayerInRange)
+        {
+            RotateTowardsPlayer();
+
+            if (IsInAngularScope() && isShotReady)
+            {
+                Shoot();
+            }
+
+            isPlayerInRange = false;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            target = other.gameObject;
+            isPlayerInRange = true;
+        }
+    }
+
+    private void RotateTowardsPlayer()
+    {
+        var direction = target.transform.position - TurretGun.transform.position;
+        direction.Normalize();
+
+        TurretGun.transform.rotation = Quaternion.Slerp(TurretGun.transform.rotation, Quaternion.LookRotation(direction), RotationAngularSpeed * Time.deltaTime);
+    }
+
+    private bool IsInAngularScope()
+    {
+        var angle = Vector3.Angle(TurretGun.transform.forward, target.transform.position - TurretGun.transform.position);
+        print(angle);
+        return angle < ShootingAngle;
+    }
+
+    private void Shoot()
+    {
+        Transform projectile = Instantiate(Projectile.transform, ProjectileSpawnPoint.transform.position, Quaternion.identity);
+        projectile.rotation = ProjectileSpawnPoint.transform.rotation;
+        isShotReady = false;
+        StartCoroutine(ShootingRate());
+    }
+
+    IEnumerator ShootingRate()
+    {
+        yield return new WaitForSeconds(FiringSpeed);
+        isShotReady = true;
+    }
+}
---
+++ Apollo3test/Assets/Turret/Scripts/TurretProjectile.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretProjectile : MonoBehaviour
+{
+    /// <summary>
+    /// Speed of the projectile.
+    /// </summary>
+    public float ProjectileSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(transform.up * Time.deltaTime * ProjectileSpeed, Space.World);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            // Deal damage to player HERE
+        }
+
+        Destroy(gameObject);
+    }
+}
