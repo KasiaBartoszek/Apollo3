e0aa418c6e757e07537fc54f77ae149af666c28f
kamfel <kamil.fel76@gmail.com>
2020-07-28 20:20:12 +0200

    Dodanie wstecznego

---
+++ Apollo3test/Assets/Script/BackCamera.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+/// <summary>
+/// Responsible for updating the back camera.
+/// </summary>
+public class BackCamera : MonoBehaviour
+{
+    public Camera BackCameraInstance;
+    public KeyCode EnableButton;
+
+    private void Start()
+    {
+        BackCameraInstance.enabled = false;
+    }
+
+    void Update()
+    {
+        var input = Input.GetKey(EnableButton);
+
+        BackCameraInstance.enabled = input;
+
+        if (input == true)
+        {
+            var mainCameraTransform = Camera.main.transform;
+            BackCameraInstance.transform.forward = -mainCameraTransform.forward;
+        }
+    }
+}
---
+++ Apollo3test/Assets/Script/BackCameraPositioning.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Applies b
+/// </summary>
+public class BackCameraPositioning : MonoBehaviour
+{
+    public Camera BackCamera;
+    public RawImage BackCameraTarget;
+    public float TargetX;
+    public float HiddenX;
+    public float SlidingSpeed;
+
+    private float currentX;
+    private bool IsVisible;
+
+    private void Start()
+    {
+        currentX = HiddenX;
+        UpdateTargetPostion();
+    }
+
+    void Update()
+    {
+        if (BackCamera.enabled)
+        {
+            if (currentX < TargetX)
+            {
+                currentX += SlidingSpeed * Time.deltaTime;
+                UpdateTargetPostion();
+            }
+        }
+        else
+        {
+            if (currentX > HiddenX)
+            {
+                currentX -= 2 * SlidingSpeed * Time.deltaTime;
+                UpdateTargetPostion();
+            }
+        }
+    }
+
+    private void UpdateTargetPostion()
+    {
+        BackCameraTarget.transform.localPosition = new Vector3(currentX, BackCameraTarget.transform.localPosition.y);
+    }
+}
