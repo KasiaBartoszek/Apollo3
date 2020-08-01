48151723002ebeec1f267d2280585f55f1705f77
furion6 <furionn6@gmail.com>
2020-08-01 15:36:12 +0200

    added oxygen
    added oxygen

---
+++ Apollo3test/Assets/Oxygen/Oxygen.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oxygen : MonoBehaviour
+{
+    // Start is called before the first frame updatepublic int maxHP;
+    
+
+    public float maxOxygen;
+    public float speedOfBreathing;
+    private float actualOxygen;
+
+    private OxygenBar oxygenBar;
+    void Start()
+    {
+        actualOxygen = maxOxygen;
+
+        oxygenBar = GetComponent<OxygenBar>();
+        if(oxygenBar != null)
+        {
+            oxygenBar.initSlider(0,maxOxygen,actualOxygen);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        breath();
+        setOxygenBar();
+        if(!hasOxygen())
+        {
+                GameManager gm = GameObject.FindWithTag("GameManager").GetComponent<GameManager>();
+                gm.gameOver();
+        }
+    }
+
+
+    private void breath()
+    {
+        actualOxygen -= speedOfBreathing * Time.deltaTime;
+    }
+
+    private bool hasOxygen()
+    {
+        return actualOxygen > 0.0f;
+    }
+
+
+
+    private void setOxygenBar()
+    {
+        if(oxygenBar != null)
+            oxygenBar.setSlider(actualOxygen);
+
+    }
+}
---
+++ Apollo3test/Assets/Oxygen/OxygenBar.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OxygenBar : MonoBehaviour
+{
+    public Slider slider;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         
+    }
+
+    
+    public void initSlider(float minValue, float maxValue, float actualValue)
+    {
+        slider.minValue = minValue;
+        slider.maxValue = maxValue;
+        slider.value = actualValue;
+    }
+
+    public void setSlider(float val)
+    {
+        slider.value = val;
+    }
+}
